2450480b9e0e6cfaa1a97e5d95df9f016be5fbd1
JALG27 <41845529+JALG27@users.noreply.github.com>
2023-03-28 13:05:54 -0400

    JL

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response} from 'express'
+import User, { IUser } from '../models/user'
+import jwt from 'jsonwebtoken'
+import config from '../config/config'
+import bcrypt from "bcrypt"
+
+function createToken(user: IUser) {
+    return jwt.sign({id: user.id, email: user.email}, config.jwtSecret, {
+        expiresIn: 86400 //24 horas o 1 dia
+    });
+}
+
+//controlador de registro
+
+export const signUp = async (req: Request, res: Response): Promise<Response> => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({msg: 'Porfavor, envia tu email y tu password'})
+    }
+    
+    const user = await User.findOne({email: req.body.email});
+    console.log(user)
+    if(user) {
+        return res.status(400).json({msg: 'El usuario ya existe'});
+    }
+    const newUser = new User(req.body);
+    await newUser.save();
+    return res.status(201).json(newUser);
+}
+
+//controlador de login
+
+export const signIn = async (req: Request, res: Response): Promise<Response> => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({msg: 'Porfavor, envia tu email y tu password'})
+    }
+
+    const user = await User.findOne({email: req.body.email})
+    if(!user) {
+        return res.status(400).json({msg: 'El usuario no existe'});
+    }
+    const isMatch = await user.comparePassword(req.body.password)
+    if (isMatch) {
+        return res.status(200).json({token: createToken(user)})
+    }
+
+    return res.status(400).json({
+        msg: 'El email o el password son incorrectos'
+    });
+}
